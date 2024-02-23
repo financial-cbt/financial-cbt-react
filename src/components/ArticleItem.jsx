ab4e24d8f4ec21fdd26cd39cfee92317c6592d6a
chanjin1998 <chanjin@kookmin.ac.kr>
2024-02-23 11:31:10 +0900

    style: article card item

--- src/components/ArticleItem.jsx
+++ src/components/ArticleItem.jsx
@@ -1,38 +1,69 @@
-import React, { useState, useCallback, useEffect } from "react";
-import styled from "styled-components";
+import React from "react";
+import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
-export default function ArticleItem({ newArticle }) {
-  const { articleId, title, content, photoUrl } = newArticle;
-  return (
-    <Container>
-      {/* <div className="row"> */}
-      <div>
-        <Link to={`${articleId}`}>
-          <div className="card h-100" style={{ width: "100%" }}>
-            <p className="card-title">{title}</p>
-            <img
-              src={photoUrl}
-              className="card-img-top"
-              alt={title}
-              style={{ padding: 30 }}
-            />
-            <div className="card-body">
-              <p className="card-text">{content}</p>
-            </div>
-          </div>
-        </Link>
-      </div>
-      {/* </div> */}
-    </Container>
-  );
-}
 
-const Container = styled.div`
-  flex: 0 0 33%; /* flex-basis를 100%로 설정 */
-  min-width: 33%; /* 최대 너비도 100%로 설정 */
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  margin: 0;
-  padding: 30px; /* 각 열 사이의 간격을 위해 padding 추가 */
+const CardLink = styled(Link)`
+  text-decoration: none;
+  &:hover {
+    transform: scale(1.05);
+    transition: transform 0.5s ease-in-out;
+  }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  margin: 40px 40px 0 40px;
+`;
+
+const ArticleItem = ({ newArticle }) => {
+  const { articleId, title, content, photoUrl } = newArticle;
+
+  return (
+    <Container>
+      <CardLink to={`${articleId}`}>
+        <div
+          className="card"
+          style={{
+            width: "400px",
+            height: "450px",
+            gap: "30px",
+            alignItems: "center",
+            // border: "2px solid rgba(0, 0, 0, 0.175)",
+          }}
+        >
+          <img
+            src={photoUrl}
+            className="card-img-top"
+            alt={title}
+            style={{ height: "300px" }}
+          />
+          <p
+            style={{
+              fontSize: "20px",
+              width: "90%",
+              height: "30px",
+              overflow: "hidden",
+              fontWeight: "600",
+            }}
+            className="card-title"
+          >
+            {title}
+          </p>
+          <div
+            style={{
+              width: "90%",
+              padding: "0",
+              height: "23px",
+              overflow: "hidden",
+            }}
+          >
+            <p style={{ fontSize: "15px", textAlign: "left" }}>{content}</p>
+          </div>
+        </div>
+      </CardLink>
+    </Container>
+  );
+};
+
+export default ArticleItem;
